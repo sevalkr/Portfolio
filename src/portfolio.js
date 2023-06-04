a428e53b6c730e599605860cc4431cb8483114eb
sevalkr <sevalkaraman_653@hotmail.com>
2023-06-04 20:06:48 +0300

    packages

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,6 +1,6 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: 'https://rajshekhar26.github.io/cleanfolio',
+  homepage: 'https://sevalkr.github.io/portfolio',
   title: 'JS.',
 }
 

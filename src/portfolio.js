282b5930fd514072fb39fb3980c8b6112bc44b17
sevalkr <sevalkaraman_653@hotmail.com>
2023-06-04 23:49:03 +0300

    links added

--- src/portfolio.js
+++ src/portfolio.js
@@ -21,12 +21,10 @@ const about = {
   // all the properties are optional - can be left empty or deleted
   name: 'Seval Karaman',
   role: 'Game Developer',
-  description:
-    'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
   resume: 'https://example.com',
   social: {
-    linkedin: 'https://linkedin.com',
-    github: 'https://github.com',
+    linkedin: 'https://www.linkedin.com/in/seval-karaman-379276159/',
+    github: 'https://github.com/sevalkr',
   },
 }
 

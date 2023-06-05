86338b828cfe3b36fa64495fcc358ed3378264f6
sevalkr <sevalkaraman_653@hotmail.com>
2023-06-05 22:18:12 +0300

    games added

--- src/portfolio.js
+++ src/portfolio.js
@@ -9,7 +9,14 @@ import HelixSortAppIcon from './media/appIcons/HelixSortAppIcon.png';
 import MineSurvivorAppIcon from './media/appIcons/MineSurvivorAppIcon.png';
 import OneManAppIcon from './media/appIcons/OneManAppIcon.png';
 import ShooterFieldAppIcon from './media/appIcons/ShooterFieldAppIcon.png';
-
+import coloridleIcon from './media/appIcons/ringlight.png';
+import coloraidIcon from './media/appIcons/coloraid.png';
+import rightChoiceIcon from './media/appIcons/rightchoice.png';
+import streamerIcon from './media/appIcons/streamer.png';
+import moneyqueenIcon from './media/appIcons/moneyqueen.png';
+import mafiaIcon from './media/appIcons/mafia.png';
+import tapeartIcon from './media/appIcons/tapeart.png';
+import jellyIcon from './media/appIcons/jelly.png';
 
 const header = {
   // all the properties are optional - can be left empty or deleted
@@ -262,6 +269,137 @@ const projects = [
       "Development platform: Unity",
       "Development time: 2 weeks",
 
+  ],
+},
+
+
+{
+  name: 'Capo Runner',
+  short_description: 'Capo Runner is the game of crowd control with basic mathematical choices.',
+  description:
+      'Capo Runner is the game of crowd control with basic mathematical choices.' +
+      'Collect the money, grow your gang in size and skill, bribe your opponents, buy enterprises and fight the enemy gang to capture the streets.' +
+      'You control a young candidate for the throne of the godfather, starting as a simple street thug, pave the way up to the highest boss rank by commanding and supporting your henchman and retainers.',
+
+  sourceCode: 'https://github.com',
+  livePreview: 'https://github.com',
+  appIcon: mafiaIcon,
+  videoUrl: "https://vimeo.com/742429636",
+  details: [
+      "Development platform: Unity",
+      "Development time: 2 week",
+  ],
+},
+{
+  name: 'Jelly Escape',
+  short_description: 'The goal is to remove jelly balls from the board by pulling them with sticks.',
+  description:
+      'The goal is to remove jelly balls from the board by pulling them with sticks.',
+
+  sourceCode: 'https://github.com',
+  livePreview: 'https://github.com',
+  appIcon: jellyIcon,
+  videoUrl: "https://vimeo.com/742494501",
+  details: [
+      "Development platform: Unity",
+      "Development time: 2 week",
+  ],
+},
+{
+  name: 'Ring Light Flip',
+  short_description: 'Flip your ring light to transform your influencers visual style!',
+  description:
+      'Flip your ring light to transform your influencers visual style!' +
+      'Flip your ring light for changing different influencer make up and look.' +
+      'Do not forget to avoid red lights while you try to become the best trend setter. Follow the light and you will become number one easily !',
+
+  sourceCode: 'https://github.com',
+  livePreview: 'https://github.com',
+  appIcon: coloridleIcon,
+  videoUrl: "https://vimeo.com/742429213",
+  details: [
+      "Development platform: Unity",
+      "Development time: 1 week",
+  ],
+},
+{
+  name: 'Tape Art Puzzle',
+  short_description: 'The goal is to try to paint the canvas the same as the target canvas.',
+  description:
+      'The goal is to try to paint the canvas the same as the target canvas.',
+
+  sourceCode: 'https://github.com',
+  livePreview: 'https://github.com',
+  appIcon: tapeartIcon,
+  videoUrl: "https://vimeo.com/742489420",
+  details: [
+      "Development platform: Unity",
+      "Development time: 2 week",
+  ],
+},
+
+{
+  name: 'Coloraid 3D',
+  short_description: 'Dig the sand and manage your army!',
+  description:
+      'Dig the sand and manage your army! They will follow the path that you created for them. You should think about the path because if you dig wrong you can lose your army. At the end of the levels, giant waiting for you!',
+
+  sourceCode: 'https://github.com',
+  livePreview: 'https://github.com',
+  appIcon: coloraidIcon,
+  videoUrl: 'https://vimeo.com/742427372',
+  details: [
+      "Development platform: Unity",
+      "Development time: 2 week",
+  ],
+},
+{
+  name: 'Streamer Prank',
+  short_description: 'Trip Prank, Wax Prank, Bottle Prank and all other jokes that will make you the most famous streamer.',
+  description:
+      'Trip Prank, Wax Prank, Bottle Prank and all other jokes that will make you the most famous streamer.' +
+      'If you prank your wife too much, she will get bored and leave you. If you want him to love you you have to give him red hearts and make him happy.',
+
+  sourceCode: 'https://github.com',
+  livePreview: 'https://github.com',
+  appIcon: streamerIcon,
+  videoUrl: "https://vimeo.com/742428140",
+  details: [
+      "Development platform: Unity",
+      "Development time: 2 week",
+  ],
+},
+
+{
+  name: 'Right Choice',
+  short_description: 'Your choices will lead them to heaven or hell. Get ready to set preferences for Dad, Sister, Brother.',
+  description:
+      'Your choices will lead them to heaven or hell. Get ready to set preferences for Dad, Sister, Brother.',
+
+  sourceCode: 'https://github.com',
+  livePreview: 'https://github.com',
+  appIcon: rightChoiceIcon,
+  videoUrl: "https://vimeo.com/742415409",
+  details: [
+      "Development platform: Unity",
+      "Development time: 2 week",
+  ],
+},
+{
+  name: 'Money Queen',
+  short_description: 'Make a big roll and wear the prettiest dresses.',
+  description:
+      'The only way to wear the prettiest dress is to create the biggest roll.' +
+
+      'Get ready for a dream journey with the most famous brands.',
+
+  sourceCode: 'https://github.com',
+  livePreview: 'https://github.com',
+  appIcon: moneyqueenIcon,
+  videoUrl: "https://vimeo.com/742430177",
+  details: [
+      "Development platform: Unity",
+      "Development time: 2 week",
   ],
 },
 ]

2baeaa2c23c9b5581961bc9b7881155e283238da
sevalkaraman <133983408+sevalkr@users.noreply.github.com>
2023-08-07 20:27:59 +0300

    email update

--- src/portfolio.js
+++ src/portfolio.js
@@ -460,7 +460,7 @@ const skills = [
 
 const contact = {
   // email is optional - if left empty Contact section won't show up
-  email: 'seval_karaman653@hotmail.com',
+  email: 'sevalkaraman653@gmail.com',
 }
 
 export { header, about, projects, skills, contact }
